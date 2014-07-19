e0fc4b426f2e06107a58480ff44c19327baa2660
Cole <pokemario5@gmail.com>
2014-07-19 13:26:55 -0400

    the hardest program I did

---
+++ sky the squid.py
@@ -0,0 +1,12 @@
+print("hello world")
+name =input("what is your name")
+print (name + " deadlox")
+
+age =input("how old are you")
+print ("you are "+ age + " years old")
+
+age = int(age)
+
+daysold = age * 365.242
+
+print("you are " +str(daysold) + " days old.")

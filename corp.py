b910866e2a4767440eddddee5a00abfe911eae02
cole andrew moore <pokemario5@gmail.com>
2014-08-23 13:47:26 -0400

    longest python EVER!!!!!!!!!!!!!!!!!!!

---
+++ corp.py
@@ -0,0 +1,21 @@
+print("hello world!")
+
+name = input("what is your name")
+
+print("hello",name)
+
+mods =input("what mods did you do")
+
+
+print("you made the",mods)
+
+waffles = input("do like waffles")
+
+
+
+print("yes we like",waffles)
+
+if waffles == 'no':
+    print("No we dont like waffles")
+      
+

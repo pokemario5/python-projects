0c97dde47a5d9c30a851f352367b74c348c06397
cole andrew moore <pokemario5@gmail.com>
2014-09-27 14:08:27 -0400

    muddy the artist

---
+++ muddy.py
@@ -0,0 +1,18 @@
+import turtle
+
+muddy = turtle.Turtle()
+muddy.pensize(10)
+muddy.pencolor('gold')
+muddy.forward(200)
+muddy.left(90)
+muddy.forward(41)
+muddy.left(90)
+muddy.forward(200)
+muddy.left(90)
+muddy.forward(41)
+
+
+
+input()
+
+
